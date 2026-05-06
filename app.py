030e3586dc043ad4f6f42dd7d4a722515ff30086
KKo-KKodac <ksk01090@gmail.com>
2026-05-06 16:38:55 +0900

    Update app.py
    SSD, HDD 추가

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def load_data_trigger():
     st.cache_data.clear()
     st.session_state['data_loaded'] = True 
 
-# 5. 데이터 수집 (요청하신 주소 3곳 추가 완료)
+# 5. 데이터 수집 (요청하신 신규 주소 6곳 추가 완료)
 @st.cache_data(ttl=3600)
 def fetch_data():
     URLS = [
@@ -115,9 +115,15 @@ def fetch_data():
         "https://www.worldmemory.co.kr/price/computer.do?ctgry_no1=8&ctgry_no2=24&ctgry_no3=4072",
         "https://www.worldmemory.co.kr/price/computer.do?ctgry_no1=8&ctgry_no2=24&ctgry_no3=4138",
         "https://www.worldmemory.co.kr/price/computer.do?ctgry_no1=8&ctgry_no2=24&ctgry_no3=4197",
-        "https://www.worldmemory.co.kr/price/computer.do?ctgry_no1=1&ctgry_no2=2&ctgry_no3=651", # 추가
-        "https://www.worldmemory.co.kr/price/computer.do?ctgry_no1=1&ctgry_no2=2&ctgry_no3=5",   # 추가
-        "https://www.worldmemory.co.kr/price/computer.do?ctgry_no1=12&ctgry_no2=42&ctgry_no3=1208" # 추가
+        "https://www.worldmemory.co.kr/price/computer.do?ctgry_no1=1&ctgry_no2=2&ctgry_no3=651",
+        "https://www.worldmemory.co.kr/price/computer.do?ctgry_no1=1&ctgry_no2=2&ctgry_no3=5",
+        "https://www.worldmemory.co.kr/price/computer.do?ctgry_no1=12&ctgry_no2=42&ctgry_no3=1208",
+        "https://www.worldmemory.co.kr/price/computer.do?ctgry_no1=4273&ctgry_no2=4274", # 추가
+        "https://www.worldmemory.co.kr/price/computer.do?ctgry_no1=4273&ctgry_no2=4275", # 추가
+        "https://www.worldmemory.co.kr/price/computer.do?ctgry_no1=4273&ctgry_no2=4276", # 추가
+        "https://www.worldmemory.co.kr/price/computer.do?ctgry_no1=4273&ctgry_no2=4280", # 추가
+        "https://www.worldmemory.co.kr/price/computer.do?ctgry_no1=4277&ctgry_no2=4278", # 추가
+        "https://www.worldmemory.co.kr/price/computer.do?ctgry_no1=4277&ctgry_no2=4279"  # 추가
     ]
     all_rows = []
     for url in URLS:
